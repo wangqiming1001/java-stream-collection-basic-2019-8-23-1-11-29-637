93601a8d0135e96597586f25807de1e56cb4c266
wangqiming1001 <512821390@qq.com>
2019-08-25 17:24:30 +0800

    add

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -1,19 +1,51 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+    	//求leftBorder 和rightBorder之间的偶数和
+    	int sum = 0;
+    	if (leftBorder < rightBorder) {
+    		for(int i = leftBorder;i<=rightBorder;i++) {
+    			if (i%2 == 0) {
+    				sum = sum+i;
+    			}
+    		}
+    	}else if (leftBorder > rightBorder) {
+    		for(int i =rightBorder ;i<=leftBorder;i++) {
+    			if (i%2 == 0) {
+    				sum = sum+i;
+    			}
+    		}
+		}else {
+			return -1;
+		}
+    	System.out.println(sum);
+    	return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    	int sum = 0;
+    	for(int i=0;i<arrayList.size();i++ ) {
+    		//求数组中每个元素的3倍加2的和
+    		sum += arrayList.get(i) * 3 + 2;
+    	}
+    	return sum;
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    	//求数组中所有偶数的平均数
+    	int sum = 0;
+    	int count = 0;
+    	for(int i=0;i<arrayList.size();i++ ) {
+    		if (arrayList.get(i)%2 == 0) {
+    			count ++;
+				sum += arrayList.get(i);
+			}
+    	}
+    	double avg = sum / count;
+    	return avg;
     }
 }
